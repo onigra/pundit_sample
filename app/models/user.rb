7ed743c0a50ff516c4be7f79b18667577d5ff5a2
onigra <3280467rec@gmail.com>
2014-04-05 16:43:32 +0900

    add User#ability method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   belongs_to :role
+
+  def ability(h = {})
+    role.roles_abilities.group_by {|i| i.ability.domain }.each do |key, vals|
+      h[key] = vals.map {|val| val.ability.ability }
+    end
+
+    return h
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,34 @@
 require 'spec_helper'
 
 describe User do
+  let(:administrator) { Fabricate(:administrator) }
+  let(:has_user_authority) { Fabricate(:has_user_authority) }
+  let(:has_role_authority) { Fabricate(:has_role_authority) }
+
   describe 'relationship' do
     it { should belong_to(:role) }
+  end
+
+  describe '#ability' do
+    context "administrator" do
+      subject { administrator.ability }
+
+      it { should include 'admin' }
+      its(['admin']) { should include 'admin' }
+    end
+
+    context "has_user_authority" do
+      subject { has_user_authority.ability }
+
+      it { should include 'user' }
+      its(['user']) { should include('index', 'show', 'create', 'update', 'destroy') }
+    end
+
+    context "has_role_authority" do
+      subject { has_role_authority.ability }
+
+      it { should include 'role' }
+      its(['role']) { should include('index', 'show', 'create', 'update', 'destroy') }
+    end
   end
 end
