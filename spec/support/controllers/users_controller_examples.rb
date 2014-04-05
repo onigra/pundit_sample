49ed2268ab03d48ca332edabd56bb1b55b069846
onigra <3280467rec@gmail.com>
2014-04-05 20:14:48 +0900

    権限によってテストを分けるためにshared_examples化

---
+++ spec/support/controllers/users_controller_examples.rb
@@ -0,0 +1,167 @@
+#
+# index
+#
+shared_examples "User indexができる" do
+  before { get :index }
+  subject { assigns(:users) }
+
+  it { should eq [user] }
+  it_behaves_like 'http code', 200
+end
+
+shared_examples "User indexができない" do
+  before { get :index }
+
+  it_behaves_like 'http code', 404
+end
+
+=begin
+#
+# show 
+#
+shared_examples "User showができる" do
+  context "GET show" do
+    it "assigns the requested user as @user" do
+      user = User.create! valid_attributes
+      get :show, {:id => user.to_param}, valid_session
+      assigns(:user).should eq(user)
+    end
+  end
+end
+
+#
+# new
+#
+shared_examples "User newができる" do
+  context "GET new" do
+    it "assigns a new user as @user" do
+      get :new, {}, valid_session
+      assigns(:user).should be_a_new(User)
+    end
+  end
+end
+
+#
+# edit
+#
+shared_examples "User editができる" do
+  context "GET edit" do
+    it "assigns the requested user as @user" do
+      user = User.create! valid_attributes
+      get :edit, {:id => user.to_param}, valid_session
+      assigns(:user).should eq(user)
+    end
+  end
+end
+
+#
+# create
+#
+shared_examples "User createができる" do
+  context "POST create" do
+    context "with valid params" do
+      it "creates a new User" do
+        expect {
+          post :create, {:user => valid_attributes}, valid_session
+        }.to change(User, :count).by(1)
+      end
+
+      it "assigns a newly created user as @user" do
+        post :create, {:user => valid_attributes}, valid_session
+        assigns(:user).should be_a(User)
+        assigns(:user).should be_persisted
+      end
+
+      it "redirects to the created user" do
+        post :create, {:user => valid_attributes}, valid_session
+        response.should redirect_to(User.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved user as @user" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        User.any_instance.stub(:save).and_return(false)
+        post :create, {:user => { "name" => "invalid value" }}, valid_session
+        assigns(:user).should be_a_new(User)
+      end
+
+      it "re-renders the 'new' template" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        User.any_instance.stub(:save).and_return(false)
+        post :create, {:user => { "name" => "invalid value" }}, valid_session
+        response.should render_template("new")
+      end
+    end
+  end
+end
+
+#
+# update
+#
+shared_examples "User updateができる" do
+  context "PUT update" do
+    context "with valid params" do
+      it "updates the requested user" do
+        user = User.create! valid_attributes
+        # Assuming there are no other users in the database, this
+        # specifies that the User created on the previous line
+        # receives the :update_attributes message with whatever params are
+        # submitted in the request.
+        User.any_instance.should_receive(:update).with({ "name" => "MyString" })
+        put :update, {:id => user.to_param, :user => { "name" => "MyString" }}, valid_session
+      end
+
+      it "assigns the requested user as @user" do
+        user = User.create! valid_attributes
+        put :update, {:id => user.to_param, :user => valid_attributes}, valid_session
+        assigns(:user).should eq(user)
+      end
+
+      it "redirects to the user" do
+        user = User.create! valid_attributes
+        put :update, {:id => user.to_param, :user => valid_attributes}, valid_session
+        response.should redirect_to(user)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns the user as @user" do
+        user = User.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
+        User.any_instance.stub(:save).and_return(false)
+        put :update, {:id => user.to_param, :user => { "name" => "invalid value" }}, valid_session
+        assigns(:user).should eq(user)
+      end
+
+      it "re-renders the 'edit' template" do
+        user = User.create! valid_attributes
+        # Trigger the behavior that occurs when invalid params are submitted
+        User.any_instance.stub(:save).and_return(false)
+        put :update, {:id => user.to_param, :user => { "name" => "invalid value" }}, valid_session
+        response.should render_template("edit")
+      end
+    end
+  end
+end
+
+#
+# destroy
+#
+shared_examples "User destroyができる" do
+  context "DELETE destroy" do
+    it "destroys the requested user" do
+      user = User.create! valid_attributes
+      expect {
+        delete :destroy, {:id => user.to_param}, valid_session
+      }.to change(User, :count).by(-1)
+    end
+
+    it "redirects to the users list" do
+      user = User.create! valid_attributes
+      delete :destroy, {:id => user.to_param}, valid_session
+      response.should redirect_to(users_url)
+    end
+  end
+end
+=end
