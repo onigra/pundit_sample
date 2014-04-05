bfe7a72747d354eff17a3c4fc7511e19e16cfb40
onigra <3280467rec@gmail.com>
2014-04-05 14:27:41 +0900

    scaffold role

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PunditSample::Application.routes.draw do
+  resources :roles
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405052429) do
+ActiveRecord::Schema.define(version: 20140405052642) do
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
