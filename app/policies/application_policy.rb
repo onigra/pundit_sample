b97eb5c5edf5e373599aa398c25958dd69d31770
onigra <3280467rec@gmail.com>
2014-04-05 17:01:58 +0900

    add policies

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,45 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    scope.where(:id => record.id).exists?
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  def can?(ability)
+    (user.ability.include?(record.class.to_s.underscore) && user.ability[record.class.to_s.underscore].include?(ability))
+  end
+end
+
---
+++ app/policies/role_policy.rb
@@ -0,0 +1,35 @@
+class RolePolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    user.admin? or can? "index"
+  end
+
+  def show?
+    user.admin? or can? "show"
+  end
+
+  def create?
+    user.admin? or can? "create"
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.admin? or can? "update"
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.admin? or can? "destroy"
+  end
+end
