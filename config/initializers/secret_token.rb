5f8e85a0819ad28f9970cfedec7081a982f2ef62
onigra <3280467rec@gmail.com>
2014-04-05 13:57:32 +0900

    this is it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PunditSample::Application.config.secret_key_base = '4e6e4f4c15676d2dadf5150d4fc3118996666267d702b58404ea74bbde6adba4d52f45597c9ee9c66b31021beb437917e005faf28784d176d1f06ad9254a9ca3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PunditSample::Application.config.session_store :cookie_store, key: '_pundit_sample_session'
