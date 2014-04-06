d40c19842738909bf5dbcd869268a9bdb23a11a7
onigra <3280467rec@gmail.com>
2014-04-07 01:08:55 +0900

    login, logoutのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 PunditSample::Application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions]
+
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root to: "home#index"
   resources :roles
   resources :users
