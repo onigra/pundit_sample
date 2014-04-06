f1c79b914787c06eb671bc255af111eee639f01a
onigra <3280467rec@gmail.com>
2014-04-06 23:25:39 +0900

    home controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PunditSample::Application.routes.draw do
   devise_for :users
-  root to: "users#index"
+  root to: "home#index"
   resources :roles
   resources :users
   get '*not_found' => 'application#render_404'
