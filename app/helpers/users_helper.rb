4607b49de18f568b337b66df9ef708b1365dcb0c
onigra <3280467rec@gmail.com>
2014-04-07 23:03:59 +0900

    user viewのpundit実装サンプル index.htmlをslimに修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def user_show?(user = current_user)
+    Pundit.policy(user, User.new).show?
+  end
+
+  def user_edit?(user = current_user)
+    Pundit.policy(user, User.new).edit?
+  end
+
+  def user_destroy?(user = current_user)
+    Pundit.policy(user, User.new).destroy?
+  end
+
+  def user_create?(user = current_user)
+    Pundit.policy(user, User.new).create?
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,4 +1,83 @@
 require 'spec_helper'
 
 describe UsersHelper do
+  describe ".user_show?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { user_show?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "User"
+      subject { user_show?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "Role"
+      subject { user_show?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".user_edit?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { user_edit?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "User"
+      subject { user_edit?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "Role"
+      subject { user_edit?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".user_destroy?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { user_destroy?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "User"
+      subject { user_destroy?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "Role"
+      subject { user_destroy?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".user_create?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { user_create?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "User"
+      subject { user_create?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "Role"
+      subject { user_create?(user) }
+      it { should be_false }
+    end
+  end
 end
