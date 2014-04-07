5a437d875e76f30f0284d57dd3a46c3c48b7f45c
onigra <3280467rec@gmail.com>
2014-04-07 23:43:51 +0900

    role viewの実装サンプル

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,2 +1,17 @@
 module RolesHelper
+  def role_show?(user = current_user)
+    Pundit.policy(user, Role.new).show?
+  end
+
+  def role_edit?(user = current_user)
+    Pundit.policy(user, Role.new).edit?
+  end
+
+  def role_destroy?(user = current_user)
+    Pundit.policy(user, Role.new).destroy?
+  end
+
+  def role_create?(user = current_user)
+    Pundit.policy(user, Role.new).create?
+  end
 end
--- spec/helpers/roles_helper_spec.rb
+++ spec/helpers/roles_helper_spec.rb
@@ -1,4 +1,83 @@
 require 'spec_helper'
 
 describe RolesHelper do
+  describe ".role_show?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { role_show?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "Role"
+      subject { role_show?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "User"
+      subject { role_show?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".role_edit?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { role_edit?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "Role"
+      subject { role_edit?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "User"
+      subject { role_edit?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".role_destroy?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { role_destroy?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "Role"
+      subject { role_destroy?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "User"
+      subject { role_destroy?(user) }
+      it { should be_false }
+    end
+  end
+
+  describe ".role_create?" do
+    context '管理者' do
+      include_context "管理者"
+      subject { role_create?(user) }
+      it { should be_true }
+    end
+
+    context '権限保持者' do
+      include_context "Role"
+      subject { role_create?(user) }
+      it { should be_true }
+    end
+
+    context '権限非保持者' do
+      include_context "User"
+      subject { role_create?(user) }
+      it { should be_false }
+    end
+  end
 end
