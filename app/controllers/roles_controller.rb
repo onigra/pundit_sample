68812a04876d9628904d0985ebb76a8bfc0bd69d
onigra <3280467rec@gmail.com>
2014-05-16 19:20:33 +0900

    administratorの権限はいじれないようにする

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,7 +4,7 @@ class RolesController < ApplicationController
   before_action :set_role, only: [:show, :edit, :update, :destroy]
 
   def index
-    @roles = Role.all
+    @roles = Role.except_admin
   end
 
   def show
