6c3cb9ef966565ce663ab868bcb79de3e4141879
onigra <3280467rec@gmail.com>
2014-05-20 14:42:55 +0900

    ability選択画面でadminは選択できない

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,8 @@
 class Ability < ActiveRecord::Base
   has_many :roles_abilities
   has_many :roles, through: :roles_abilities
+
+  scope :except_admin, -> {
+    where.not(abilities: { domain: "admin", ability: "admin" })
+  }
 end
